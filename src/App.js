a9921a35552fb0e8f9735b71569eb3b06bd52661
ilaydasumer1 <ilayda_sumer21@outlook.com>
2024-04-12 16:56:09 +0300

    thirdcommit

--- src/App.js
+++ src/App.js
@@ -36,115 +36,140 @@ const App = () => {
     case 1:
       return (
         <div className="container">
-          <div className="container-1">
-            <h2 className="header">Tell Us About Your Requirements</h2>
-            <form onSubmit={handleSubmit} className="content">
-              <input
-                name="firstName"
-                type="text"
-                value={formData.firstName}
-                onChange={handleChange}
-                placeholder="First Name"
-              />
-              <input
-                name="lastName"
-                type="text"
-                value={formData.lastName}
-                onChange={handleChange}
-                placeholder="Last Name"
-              />
-              <input
-                name="userName"
-                type="text"
-                value={formData.userName}
-                onChange={handleChange}
-                placeholder="User Name"
-              />
-              <input
-                name="email"
-                type="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Email Address"
-              />
-              <button
-                onClick={nextStep}
-                type="button"
-                class="btn btn-primary"
-                data-toggle="button"
-                aria-pressed="false"
-                autocomplete="off"
-              >
-                Next
-              </button>
-            </form>
+        <div className="icons">
+          <span>icon1</span>
+          <span>icon2</span>
+          <span>icon3</span>
+        </div>
+          <div className="form-div">
+            <div>
+              <h4 className="header">Tell Us About Your Requirements</h4>
+            </div>
+            <div>
+              <form onSubmit={handleSubmit} className="content">
+                <input
+                  name="firstName"
+                  type="text"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  placeholder="First Name"
+                />
+                <input
+                  name="lastName"
+                  type="text"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  placeholder="Last Name"
+                />
+                <input
+                  name="userName"
+                  type="text"
+                  value={formData.userName}
+                  onChange={handleChange}
+                  placeholder="User Name"
+                />
+                <input
+                  name="email"
+                  type="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Email Address"
+                />
+                <button
+                  onClick={nextStep}
+                  type="button"
+                  class="btn btn-primary"
+                  data-toggle="button"
+                  aria-pressed="false"
+                  autocomplete="off"
+                >
+                  Next
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       );
     case 2:
       return (
-        <form onSubmit={handleSubmit}>
-          <select name="youare" value={formData.youare} onChange={handleChange}>
-            <option value="">You are</option>
-            <option value="digitalBranding">Software Developer</option>
-            <option value="photography">Industrial engineer</option>
-            <option value="photography">Professional Photographer</option>
-            <option value="webDesign">Web Designer</option>
-            <option value="webDesign">graphic artist</option>
-            <option value="other">Other</option>
-          </select>
-          <select
-            name="youhave"
-            value={formData.youhave}
-            onChange={handleChange}
-          >
-            <option value="">You have</option>
-            <option value="digitalBranding">Idea</option>
-            <option value="photography">company</option>
-            <option value="webDesign">budget</option>
-            <option value="webDesign">worker</option>
-            <option value="other">Other</option>
-          </select>
-          <select
-            name="typeOfProject"
-            value={formData.typeOfProject}
-            onChange={handleChange}
-          >
-            <option value="">Type of Project/Query</option>
-            <option value="digitalBranding">Digital Branding</option>
-            <option value="photography">Professional Photography</option>
-            <option value="webDesign">Web Design</option>
-            <option value="appDevelopment">Mobile App Development</option>
-            <option value="digitalMarketing">Digital Marketing</option>
-            <option value="other">Other</option>
-          </select>
-          <input
-            name="budget"
-            type="text"
-            value={formData.budget}
-            onChange={handleChange}
-            placeholder="Budget"
-          />
-          <button
-            onClick={prevStep}
-            type="button"
-            class="btn btn-primary"
-            data-toggle="button"
-            aria-pressed="false"
-            autocomplete="off"
-          >
-            Previous
-          </button>
-          <button
-            type="button"
-            class="btn btn-primary"
-            data-toggle="button"
-            aria-pressed="false"
-            autocomplete="off"
-          >
-            Submit
-          </button>
-        </form>
+        <div className="container">
+          <div className="form-div">
+            <div>
+              <h4 className="header">Tell Us About Your Requirements</h4>
+            </div>
+            <div>
+              <form className="content" onSubmit={handleSubmit}>
+                <select
+                  className="content-select"
+                  name="youare"
+                  value={formData.youare}
+                  onChange={handleChange}
+                >
+                  <option value="">You are</option>
+                  <option value="digitalBranding">Software Developer</option>
+                  <option value="photography">Industrial engineer</option>
+                  <option value="photography">Professional Photographer</option>
+                  <option value="webDesign">Web Designer</option>
+                  <option value="webDesign">graphic artist</option>
+                  <option value="other">Other</option>
+                </select>
+                <select
+                  className="content-select"
+                  name="youhave"
+                  value={formData.youhave}
+                  onChange={handleChange}
+                >
+                  <option value="">You have</option>
+                  <option value="digitalBranding">Idea</option>
+                  <option value="photography">company</option>
+                  <option value="webDesign">budget</option>
+                  <option value="webDesign">worker</option>
+                  <option value="other">Other</option>
+                </select>
+                <select
+                  className="content-select"
+                  name="typeOfProject"
+                  value={formData.typeOfProject}
+                  onChange={handleChange}
+                >
+                  <option value="">Type of Project/Query</option>
+                  <option value="digitalBranding">Digital Branding</option>
+                  <option value="photography">Professional Photography</option>
+                  <option value="webDesign">Web Design</option>
+                  <option value="appDevelopment">Mobile App Development</option>
+                  <option value="digitalMarketing">Digital Marketing</option>
+                  <option value="other">Other</option>
+                </select>
+                <input
+                  name="budget"
+                  type="text"
+                  value={formData.budget}
+                  onChange={handleChange}
+                  placeholder="Budget"
+                />
+                <button
+                  onClick={prevStep}
+                  type="button"
+                  class="btn btn-primary"
+                  data-toggle="button"
+                  aria-pressed="false"
+                  autocomplete="off"
+                >
+                  Previous
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  data-toggle="button"
+                  aria-pressed="false"
+                  autocomplete="off"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
       );
     default:
       return null;
